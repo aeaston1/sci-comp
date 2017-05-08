39942374f170e36fb390b1e7dda72f47c646059d
Alexander Easton <Easton.ae@gmail.com>
2017-05-08 13:53:08 +0200

    added U and V arrays

--- assign3.py
+++ assign3.py
@@ -1,4 +1,5 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 
 #intiail conditions, denoted with "_i"
 del_t = 1
@@ -10,9 +11,19 @@ k_i = 0.06
 u_i = 0.5 #initially everywhere in the system
 v_i = 0.25 #intialy in a small square in the centre of the system 0 elsewhere
 p_i = 0 #initialise this to zero
+n_rows_i = 100 #number of rows
+n_cols_i = 100 #number of columns
 
-def fill_UV(n_rows, n_cols, uv):
-    return np.full((n_rows, n_cols), u, dtype=float)
+def fill_U(u):
+    return np.full((n_rows_i, n_cols_i), u, dtype=float)
+
+def fill_V(v):
+    #create a square of chemical in the centre of the square
+    V = np.zeros((n_rows_i, n_cols_i), dtype=float)
+    for i in range(int(n_rows_i/2 - 2), int(n_rows_i/2 + 2)):
+        for j in range(int(n_cols_i/2 - 2), int(n_cols_i/2 + 2)):
+            V[i,j] = v
+    return V
 
 def u_to_v(u, v):
     new_v = u + 2*v
@@ -27,3 +38,10 @@ def u_time(d_t, D_u, u, v, f, i,j):
 
 def v_time(d_t, D_v, u, v, f, k, i, j):
     return d_t * (D_v * v[i,j]) + (u[i,j] * (v[i,j])**2) - (v * (f + k))
+
+
+if __name__ == "__main__":
+    U = fill_U(u_i)
+    V = fill_V(v_i)
+    plt.imshow(V)
+    plt.show()
