3ab4b9c6424d8c4d8eccf05544b320f24b05ee0d
Alexander Easton <Easton.ae@gmail.com>
2017-05-09 15:05:51 +0200

    status update

--- assign3.py
+++ assign3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 #intiail conditions, denoted with "_i"
 del_t = 1
@@ -13,6 +14,9 @@ v_i = 0.25 #intialy in a small square in the centre of the system 0 elsewhere
 p_i = 0 #initialise this to zero
 n_rows_i = 100 #number of rows
 n_cols_i = 100 #number of columns
+Res = np.zeros((n_rows_i, n_cols_i), dtype=float)
+P = np.zeros((n_rows_i, n_cols_i), dtype=float)
+timesteps = 10
 
 def fill_U(u):
     return np.full((n_rows_i, n_cols_i), u, dtype=float)
@@ -25,6 +29,17 @@ def fill_V(v):
             V[i,j] = v
     return V
 
+def fill_UV(u,v):
+    for i in range(n_rows_i):
+        for j in range(n_cols_i):
+            if i >= (int(n_rows_i/2 -2)) and i <= (int(n_rows_i/2 + 2)):
+                if j >= (int(n_cols_i/2 - 2)) and j <= (int(n_cols_i/2 + 2)):
+                    Res[i][j] = np.array[(u,v)]
+                else:
+                    continue
+            Res[i][j] = np.array[(u,0.0)]
+    return Res
+
 def u_to_v(u, v):
     new_v = u + 2*v
     return 3 * new_v
@@ -33,15 +48,56 @@ def v_to_p(v, p):
     p = copy.deepcopy(v)
     return p
 
-def u_time(d_t, D_u, u, v, f, i,j):
-    return d_t * (D_u * u[i,j]) - (u[i,j] * (v[i,j])**2) + (f * (1-u[i,j]))
+def u_time(d_t, D_u, U, V, f, i,j):
+    return d_t * (D_u * U[i,j]) - (U[i,j] * (V[i,j])**2) + (f * (1-U[i,j]))
 
-def v_time(d_t, D_v, u, v, f, k, i, j):
-    return d_t * (D_v * v[i,j]) + (u[i,j] * (v[i,j])**2) - (v * (f + k))
+def v_time(d_t, D_v, U, V, f, k, i, j):
+    return d_t * (D_v * V[i,j]) + (U[i,j] * (V[i,j])**2) - (V[i,j] * (f + k))
 
+def hessian(U):
+    U_grad = np.gradient(U)
+    hessian = np.empty((U.ndim, U.ndim) + U.shape, dtype=U.dtype)
+    for k, grad_k in enumerate(U_grad):
+        # iterate over dimensions
+        # apply gradient again to every component of the first derivative.
+        tmp_grad = np.gradient(grad_k)
+        for l, grad_kl in enumerate(tmp_grad):
+            hessian[k, l, :, :] = grad_kl
+    return hessian
+
+def utime(d_t, D_u, U, V, f):
+    V_sq = np.matmul(V,V)
+    du = d_t * ((D_u * hessian(U)) - (np.matmul(U, V_sq)) + (f * (1 - U)))
+    return du
+
+def vtime(d_t, D_v, U, V, f, k):
+    V_sq = np.matmul(V,V)
+    dv = d_t * ((D_v * hessian(V)) + (np.matmul(U, V_sq)) - ((f + k) * V))
+    return dv
 
 if __name__ == "__main__":
+    # general idea
+    # fill the U and V arrays with the initial values.
+    # in one time step, react U with V and output to the resultant array
+    # after the first time step the resultant array must be the only array to be worked on
+    # potentially create an array of tuples, one element is the U value and one is the V value
+    #TODO: check out the return dimensions of the hessian function
     U = fill_U(u_i)
     V = fill_V(v_i)
-    plt.imshow(V)
+    U = utime(del_t, D_u_i, U, V, f_i)
+    V = vtime(del_t, D_v_i, U, V, f_i, k_i)
+
+    for t in range(timesteps):
+        U = utime(del_t, D_u_i, U[0][0], V[0][0], f_i)
+        V = vtime(del_t, D_v_i, U[0][0], V[0][0], f_i, k_i)
+    # for t in range(timesteps):
+    #     for i in range(n_rows_i):
+    #         for j in range(n_cols_i):
+    #             U[i,j] = u_time(del_t, D_u_i, U, V, f_i, i, j)
+    #             V[i,j] = v_time(del_t, D_v_i, U, V, f_i, k_i, i, j)
+
+    fig, ax = plt.subplots(2)
+    cax0 = ax[0].imshow(U[0][0], interpolation='nearest', cmap=cm.afmhot)
+    cax1 = ax[1].imshow(V[0][0], interpolation='nearest', cmap=cm.afmhot)
+    # cbar0 = fig.colorbar(cax0)
     plt.show()
