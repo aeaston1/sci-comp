1db248c96e39200125e99b3f5c7e726843f56eac
Alexander Easton <Easton.ae@gmail.com>
2017-05-12 21:51:03 +0200

    final experiments

--- assign3.py
+++ assign3.py
@@ -2,18 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import matplotlib
-import scipy as sp
-import math
-import pandas # for printing
 import copy
 import time
-from numba import jit, autojit
-import random
-import warnings
 
 del_t = 1
 del_x = 1
-d={}
 # parameter sets
 D_u_i1, D_v_i1, f_i1, k_i1, u_i1, v_i1 = 0.16, 0.08, 0.035, 0.16, 0.5, 0.25
 Cons_u1, Cons_v1 = D_u_i1*(del_t/float(del_x**2)), D_v_i1*(del_t/float(del_x**2))
@@ -39,9 +32,10 @@ Cons_u6, Cons_v6 = D_u_i6*(del_t/float(del_x**2)), D_v_i6*(del_t/float(del_x**2)
 #v_i intialy in a small square in the centre of the system 0 elsewhere
 
 N = 100 #square size of grid
-T = 10 #number of steps in interval 0->1
+T = 300 #number of steps in interval 0->1
 
 def fill_U(u):
+    #fill the array with u value everywhere
     return np.full((N+1, N+1), u, dtype=float)
 
 def fill_V(v):
@@ -52,67 +46,12 @@ def fill_V(v):
             V[i,j] = v
     return V
 
-def fill_UV(u,v):
-    for i in range(N):
-        for j in range(N):
-            if i >= (int(N/2 -2)) and i <= (int(N/2 + 2)):
-                if j >= (int(N/2 - 2)) and j <= (int(N/2 + 2)):
-                    Res[i][j] = np.array[(u,v)]
-                else:
-                    continue
-            Res[i][j] = np.array[(u,0.0)]
-    return Res
-
-def u_to_v(u, v):
-    new_v = u + 2*v
-    return 3 * new_v
-
-def v_to_p(v, p):
-    p = copy.deepcopy(v)
-    return p
-
-def u_time(d_t, D_u, U, V, f, i,j):
-    return d_t * (D_u * U[i,j]) - (U[i,j] * (V[i,j])**2) + (f * (1-U[i,j]))
-
-def v_time(d_t, D_v, U, V, f, k, i, j):
-    return d_t * (D_v * V[i,j]) + (U[i,j] * (V[i,j])**2) - (V[i,j] * (f + k))
-
-def hessian(U):
-    U_grad = np.gradient(U)
-    hessian = np.empty((U.ndim, U.ndim) + U.shape, dtype=U.dtype)
-    for k, grad_k in enumerate(U_grad):
-        # iterate over dimensions
-        # apply gradient again to every component of the first derivative.
-        tmp_grad = np.gradient(grad_k)
-        for l, grad_kl in enumerate(tmp_grad):
-            hessian[k, l, :, :] = grad_kl
-    return hessian
-
-def utime(d_t, D_u, U, V, f):
-    V_sq = np.matmul(V,V)
-    du = d_t * ((D_u * hessian(U)) - (np.matmul(U, V_sq)) + (f * (1 - U)))
-    return du
-
-def vtime(d_t, D_v, U, V, f, k):
-    V_sq = np.matmul(V,V)
-    dv = d_t * ((D_v * hessian(V)) + (np.matmul(U, V_sq)) - ((f + k) * V))
-    return dv
-
-def grayscott_u(x,y,M_u,M_v):
-    newU[x,y] = M_u[x,y] + Cons_u*( \
-        M_u[x-1,y] + M_u[(x+1)%N,y] + M_u[x,y-1] + M_u[x,y+1] - 4*M_u[x,y]) \
-        - M_u[x,y]*(M_v[x,y])**2 + f_i * (1 - M_u[x,y])
-
-def grayscott_v(x,y,M_u,M_v):
-    newV[x,y] = M_v[x,y] + Cons_v*( \
-        M_v[x-1,y] + M_v[(x+1)%N,y] + M_v[x,y-1] + M_v[x,y+1] - 4*M_v[x,y]) \
-        - M_u[x,y]*(M_v[x,y])**2 + f_i * (f_i + k_i) * M_v[x,y]
-
 def tridiag(a, b, c, k1=-1, k2=0, k3=1):
     return np.diag(a, k1) + np.diag(b, k2) + np.diag(c, k3)
 
 def simulation(totT, mat_u, newu, u, Consu, mat_v, newv, v, Consv, f_i, k_i):
     for t in np.arange(totT):
+        #Gray-Scott equations
         newu = Consu*(TD*mat_u+mat_u*TD + Boundary*mat_u)  \
         - (u * v**2 + (f_i * (1.0 - u)))
         newv = Consv*(TD*mat_v+mat_v*TD + Boundary*mat_v)  \
@@ -134,17 +73,20 @@ if __name__ == "__main__":
         globals()["newU%s" % x] = copy.deepcopy(globals()["mat_U%s" % x])
         globals()["newV%s" % x] = copy.deepcopy(globals()["mat_V%s" % x])
 
+    #creation of tridiagonal matrices
     a = [1 for n in np.arange(N)]
     b = [0 for n in np.arange(N+1)]
     TD = tridiag(a,b,a)
     TD = np.matrix(TD)
-    # Boundary[0,N] = 1
-    # Boundary[N,0] = 1
-    # Boundary[0,0] = 1
-    # Boundary[N,N] = 1
+    # boundaries
     Boundary = np.zeros((N+1,N+1))
+    Boundary[0,N] = 1
+    Boundary[N,0] = 1
+    Boundary[0,0] = 1
+    Boundary[N,N] = 1
     Boundary = np.matrix(Boundary)
     print("Starting loop...")
+    # running multiple simulations for various parameters
     for x in range(1,7):
         globals()["newU%s" % x],globals()["newV%s" % x] = \
         simulation(T, globals()["mat_U%s" % x], \
@@ -156,20 +98,45 @@ if __name__ == "__main__":
     end = time.time()
     print(end-start)
 
-    fig, ax = plt.subplots(6,2)
-    ax[0,0].matshow(newU1, cmap=plt.cm.gray)
-    ax[0,1].matshow(newV1, cmap=plt.cm.gray)
-    ax[1,0].matshow(newU2, cmap=plt.cm.gray)
-    ax[1,1].matshow(newV2, cmap=plt.cm.gray)
-    ax[2,0].matshow(newU3, cmap=plt.cm.gray)
-    ax[2,1].matshow(newV3, cmap=plt.cm.gray)
-    ax[3,0].matshow(newU4, cmap=plt.cm.gray)
-    ax[3,1].matshow(newV4, cmap=plt.cm.gray)
-    ax[4,0].matshow(newU5, cmap=plt.cm.gray)
-    ax[4,1].matshow(newV5, cmap=plt.cm.gray)
-    ax[5,0].matshow(newU6, cmap=plt.cm.gray)
-    ax[5,1].matshow(newV6, cmap=plt.cm.gray)
+    fig, ax = plt.subplots(3,2)
+    fig1, ax1 = plt.subplots(3,2)
+    ax00 = ax[0,0].matshow(newU1, cmap=plt.cm.gray)
+    ax01 = ax[0,1].matshow(newV1, cmap=plt.cm.gray)
+    ax10 = ax[1,0].matshow(newU2, cmap=plt.cm.gray)
+    ax11 = ax[1,1].matshow(newV2, cmap=plt.cm.gray)
+    ax20 = ax[2,0].matshow(newU3, cmap=plt.cm.gray)
+    ax21 = ax[2,1].matshow(newV3, cmap=plt.cm.gray)
+    ax100 = ax1[0,0].matshow(newU4, cmap=plt.cm.gray)
+    ax101 = ax1[0,1].matshow(newV4, cmap=plt.cm.gray)
+    ax110 = ax1[1,0].matshow(newU5, cmap=plt.cm.gray)
+    ax111 = ax1[1,1].matshow(newV5, cmap=plt.cm.gray)
+    ax120 = ax1[2,0].matshow(newU6, cmap=plt.cm.gray)
+    ax121 = ax1[2,1].matshow(newV6, cmap=plt.cm.gray)
 
-    ax[0,0].set_title("U Concentration Grids")
-    ax[0,1].set_title("V Concentration Grids")
+    #colorbars
+    fig.colorbar(ax00, ax=ax[0,0])
+    fig.colorbar(ax01, ax=ax[0,1])
+    fig.colorbar(ax10, ax=ax[1,0])
+    fig.colorbar(ax11, ax=ax[1,1])
+    fig.colorbar(ax20, ax=ax[2,0])
+    fig.colorbar(ax21, ax=ax[2,1])
+    fig.colorbar(ax100, ax=ax1[0,0])
+    fig.colorbar(ax101, ax=ax1[0,1])
+    fig.colorbar(ax110, ax=ax1[1,0])
+    fig.colorbar(ax111, ax=ax1[1,1])
+    fig.colorbar(ax120, ax=ax1[2,0])
+    fig.colorbar(ax121, ax=ax1[2,1])
+
+    ax[0,0].set_title("U - (a)", fontsize=20)
+    ax[0,1].set_title("V - (a)",  fontsize=20)
+    ax[1,0].set_title("U - (b)", fontsize=20)
+    ax[1,1].set_title("V - (b)",  fontsize=20)
+    ax[2,0].set_title("U - (c)", fontsize=20)
+    ax[2,1].set_title("V - (c)",  fontsize=20)
+    ax1[0,0].set_title("U - (d)",  fontsize=20)
+    ax1[0,1].set_title("V - (d)",  fontsize=20)
+    ax1[1,0].set_title("U - (e)",  fontsize=20)
+    ax1[1,1].set_title("V - (e)",  fontsize=20)
+    ax1[2,0].set_title("U - (f)",  fontsize=20)
+    ax1[2,1].set_title("V - (f)",  fontsize=20)
     plt.show()
